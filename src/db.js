93b75a25559f6a302199675a62a37846d9f5c529
SebastianLozanoValle <cesarloco2003@hotmail.com>
2023-12-21 00:55:12 -0500

    prueba#2

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
