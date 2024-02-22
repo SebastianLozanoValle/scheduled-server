18e20ba8a487083d8e4e20d37a4edd1235b5fbcc
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2024-02-22 01:15:26 -0500

    emmmm uds no vieron eso...

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const { User } = require("./src/models/User.js");
 const bcrypt = require('bcryptjs');
 const { Client } = require("./src/models/Client.js");
 
-// const hashPassword = async (password) => {
-//     const salt = await bcrypt.genSalt(10);
-//     return await bcrypt.hash(password, salt);
-// };
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
 
-// const JWT_SECRET = 'NEVER_SHARE_THIS'
+const JWT_SECRET = 'NEVER_SHARE_THIS'
 
 const app = express();
 module.exports = { app };
@@ -37,16 +37,16 @@ module.exports = { app };
 // }
 // throw new Error(`An error occurred with directory: ${dirPath}`);
 // Configuración de multer
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, './files') // Aquí especificas el directorio donde se guardarán los archivos
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + path.extname(file.originalname)) // Aquí puedes especificar el nombre del archivo
-//     }
-// });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './files') // Aquí especificas el directorio donde se guardarán los archivos
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)) // Aquí puedes especificar el nombre del archivo
+    }
+});
 
-// const upload = multer({ storage });
+const upload = multer({ storage });
 
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -62,24 +62,24 @@ const start = async () => {
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
-        // cache: 'bounded',
-        // context: async ({ req, res }) => {
-        //     const auth = req ? req.headers.authorization : null;
-        //     if (auth && auth.toLowerCase().startsWith("bearer ")) {
-        //         const token = auth.substring(7);
-        //         const { id } = jwt.verify(token, JWT_SECRET);
-        //         let currentUser = await Specialist.findById(id)
-        //         if (!currentUser) {
-        //             currentUser = await Client.findById(id)
-        //             if (!currentUser) {
-        //                 currentUser = await User.findById(id)
-        //             }
-        //         }
-        //         return { currentUser };
-        //     }
-        //     return {};
-        // },
-        // connectToDevTools: true
+        cache: 'bounded',
+        context: async ({ req, res }) => {
+            const auth = req ? req.headers.authorization : null;
+            if (auth && auth.toLowerCase().startsWith("bearer ")) {
+                const token = auth.substring(7);
+                const { id } = jwt.verify(token, JWT_SECRET);
+                let currentUser = await Specialist.findById(id)
+                if (!currentUser) {
+                    currentUser = await Client.findById(id)
+                    if (!currentUser) {
+                        currentUser = await User.findById(id)
+                    }
+                }
+                return { currentUser };
+            }
+            return {};
+        },
+        connectToDevTools: true
     });
 
     await apolloServer.start();
@@ -88,22 +88,22 @@ const start = async () => {
 
     app.use(cors());
 
-    // app.get("/", (req, res) => {
-    //     res.send("Hello World!");
-    // });
+    app.get("/", (req, res) => {
+        res.send("Hello World!");
+    });
 
-    // // Endpoint para subir archivos
-    // app.post('/upload', upload.single('file'), (req, res) => {
-    //     // Aquí puedes manejar lo que sucede después de que el archivo se ha subido
-    //     // Por ejemplo, podrías guardar la URL del archivo en tu base de datos
-    //     res.json({ file: req.file });
-    // });
+    // Endpoint para subir archivos
+    app.post('/upload', upload.single('file'), (req, res) => {
+        // Aquí puedes manejar lo que sucede después de que el archivo se ha subido
+        // Por ejemplo, podrías guardar la URL del archivo en tu base de datos
+        res.json({ file: req.file });
+    });
 
-    // app.use("*", (req, res) => {
-    //     res.status(404).json({
-    //         error: "Not found"
-    //     });
-    // });
+    app.use("*", (req, res) => {
+        res.status(404).json({
+            error: "Not found"
+        });
+    });
 
     const port = process.env.PORT || 33402;
     app.listen(port, async () => {
